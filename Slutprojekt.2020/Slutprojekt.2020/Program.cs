550245d977c91e2cf07953b34095d89bb7724962
KnasBas <anton.jansson@elev.ga.ntig.se>
2020-03-03 13:11:03 +0100

    commit 1

---
+++ Slutprojekt.2020/Slutprojekt.2020/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+
+
+
+namespace Slutprojekt._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient client = new RestClient("https://official-joke-api.appspot.com/");
+
+            RestRequest request = new RestRequest("jokes/random");
+
+            IRestResponse response = client.Get(request);
+
+            Console.WriteLine(response.Content);
+
+            Console.ReadLine();
+        }
+    }
+}
